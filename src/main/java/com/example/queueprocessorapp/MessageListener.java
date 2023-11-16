8a0a9197299870789f8d30920c7d02eb38008b63
Mikhail Knyazev <solnseselo@yandex.ru>
2023-11-16 14:07:57 +0400

    add corrections

--- src/main/java/com/example/queueprocessorapp/MessageListener.java
+++ src/main/java/com/example/queueprocessorapp/MessageListener.java
@@ -3,8 +3,10 @@ package com.example.queueprocessorapp;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.time.Instant;
 
@@ -14,28 +16,36 @@ public class MessageListener {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @RabbitListener(queues = "test.input")
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Value("${rabbitmq.input.queue}")
+    private String inputQueue;
+
+    @Value("${rabbitmq.output.exchange}")
+    private String outputExchange;
+
+    @Value("${rabbitmq.output.routingkey}")
+    private String outputRoutingKey;
+
+    @Value("${message.processing.status}")
+    private String processingStatus;
+
+    @RabbitListener(queues = "#{@inputQueue}")
     public void receiveMessage(String message) {
         System.out.println("Received message: " + message);
 
-        ObjectMapper mapper = new ObjectMapper();
-
         try {
-            // Переделываем JSON в CustomMessage
             CustomMessage customMessage = mapper.readValue(message, CustomMessage.class);
 
-            // Добавляем дополнительные поля
             customMessage.setHandledTimestamp(Instant.now().toEpochMilli());
-            customMessage.setStatus("Complete");
+            customMessage.setStatus(processingStatus);
 
-            // Переделываем обратно в JSON
             String modifiedMessage = mapper.writeValueAsString(customMessage);
 
-            // Отправка измененного сообщения в RabbitMQ
-            rabbitTemplate.convertAndSend("test.output", "output", modifiedMessage);
+            rabbitTemplate.convertAndSend(outputExchange, outputRoutingKey, modifiedMessage);
 
         } catch (IOException e) {
-
             e.printStackTrace();
         }
     }
