215e6ebbac1edbf43926357b52c3a592a0789f5e
Mikhail Knyazev <solnseselo@yandex.ru>
2023-11-22 16:30:36 +0400

    add spring rabbitmq 2nd task

--- src/main/java/com/example/queueprocessorapp/MyCamelRouter.java
+++ src/main/java/com/example/queueprocessorapp/MyCamelRouter.java
@@ -2,15 +2,30 @@ package com.example.queueprocessorapp;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class MyCamelRouter extends RouteBuilder {
 
+    private static final Logger log = LoggerFactory.getLogger(MyCamelRouter.class);
+
+    @Bean
+    public CachingConnectionFactory rabbitConnectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
     @Override
     public void configure() {
-        // Обработка сообщений из очереди и отправка в обменник
-        from("rabbitmq:test.input") // Здесь указывается имя очереди
+        from("spring-rabbitmq:?connectionFactory=#rabbitConnectionFactory&queues=test.input")
                 .routeId("ProcessAndSendRoute")
                 .log("Received message from test.input: ${body}")
                 .unmarshal().json(JsonLibrary.Jackson, CustomMessage.class)
@@ -18,18 +33,19 @@ public class MyCamelRouter extends RouteBuilder {
                     CustomMessage message = exchange.getIn().getBody(CustomMessage.class);
                     String type = exchange.getIn().getHeader("type", String.class);
 
+                    log.info("Processing message with type: " + type);
+
                     if ("employee".equals(type)) {
                         message.setHandledTimestamp(System.currentTimeMillis());
                         message.setStatus("Complete");
                         exchange.getMessage().setBody(message);
-                        exchange.getMessage().setHeader("type", type);
                     } else {
-                        // Логирование для других типов
+                        log.warn("Received non-employee type message");
                         exchange.getMessage().setHeader("type", "non-employee");
                     }
                 })
                 .marshal().json(JsonLibrary.Jackson)
-                .to("rabbitmq:test.output?routingKey=output") // Здесь указывается имя обменника и ключ маршрутизации
+                .to("spring-rabbitmq:exchange.test.output?connectionFactory=#rabbitConnectionFactory&exchangeType=direct&routingKey=output")
                 .log("Sent message to test.output: ${body}")
                 .end();
 
